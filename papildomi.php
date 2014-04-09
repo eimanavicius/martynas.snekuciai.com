b189a43073d9f0daf224dec31c5af46a0322b862
Nerijus Eimanavičius <nermnv@telesoftas.com>
2014-04-09 11:33:34 +0300

    access control headers update

--- papildomi.php
+++ papildomi.php
@@ -66,4 +66,8 @@ if (null !== $httpReferer) {
 
 if (null !== $ref) {
     header("Access-Control-Allow-Origin: " . $ref);
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+        header("Access-Control-Allow-Headers: " . $_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']);
+    }
 }
